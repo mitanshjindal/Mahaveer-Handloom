10692351f993a87d03c1ebb8f928b4c6c9586ac5
Mitansh <mitanshjindal001@gmail.com>
2026-04-17 15:55:04 +0530

    Deploying

--- server.js
+++ server.js
@@ -111,6 +111,10 @@ app.get('/admin', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
